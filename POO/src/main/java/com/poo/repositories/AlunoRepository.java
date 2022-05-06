1ac4d6eca3644c3d9b464b9cada27db80a7ce001
HeitorNunes275 <88085970+HeitorNunes275@users.noreply.github.com>
2022-05-06 20:18:23 -0300

    Add files via upload

---
+++ POO/src/main/java/com/poo/repositories/AlunoRepository.java
@@ -0,0 +1,13 @@
+package com.poo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.poo.model.Aluno;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long>{
+
+
+}
+ 
+
+
