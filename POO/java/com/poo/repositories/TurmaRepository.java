c87c03044ad145feefabac033eb9ff5515742a72
HeitorNunes275 <88085970+HeitorNunes275@users.noreply.github.com>
2022-05-06 20:17:22 -0300

    Delete POO directory

--- POO/java/com/poo/repositories/TurmaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.poo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.poo.model.Turma;
-
-public interface TurmaRepository extends JpaRepository<Turma, Long>{
-	
-}
